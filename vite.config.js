896166c3ac0cb76ab938848dca9864aca3df0c65
Triyog <triyogshrestha86@gmail.com>
2026-06-10 12:22:43 +0545

    changes to base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/enpServices/',
+  base: '/',
   plugins: [react()],
   server: {
     historyApiFallback: true
